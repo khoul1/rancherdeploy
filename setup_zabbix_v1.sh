aebfc9a7197e311271cc5ef2cd1a3caceba261b6
khoul1 <50237378+khoul1@users.noreply.github.com>
2020-10-02 11:32:21 -0400

    Update setup_zabbix_v1.sh

--- setup_zabbix_v1.sh
+++ setup_zabbix_v1.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
 # ZabbixTest_agent_install
 # Installation script Zabbix Agent
 # Version 0.0.9
 # March 20 2020
-# Property of Purkinje
-set -e
+# Property of Purkinje Inc.
+#!/bin/bash -e
 
 ######################################################
 
 # ********* Script must be executed as root ********
 #  Ubuntu 16 + 
 
-#  This script assumes the UFW is disabled.
+#  This script assumes the UFW is enabled.
 
 ######################################################
 
@@ -29,8 +28,8 @@ set -e
 #####################################################
 
 # Set Zabbix Server IP address (ZABIP) and Communicaiton port (PORT)
-  ZABIP=PropertiesFile
-  PORT=PropertiesFile
+  ZABIP=1.2.3.4
+  PORT=123
 
 # Set Hostname
   HOSTNAME=`hostname`
@@ -40,26 +39,26 @@ set -e
 
 #  Ubuntu 16 + using UFW for firewall, this script assumes the firewall is enabled.
 if [ -x /usr/bin/apt-get ]; then
-  sudo apt-get update
-  sudo apt-get -y install zabbix-agent
-  sudo systemctl enable zabbix-agent
+  apt-get update
+  apt-get -y install zabbix-agent
+  systemctl enable zabbix-agent
 
 # Injecting custom configurations into Zabbix agent conf file.
 
-  sudo sed -i 's/# EnableRemoteCommands=0/EnableRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i 's/# LogRemoteCommands=0/LogRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i "s/Server=127.0.0.1/Server=$ZABIP/" /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i "s/ServerActive=127.0.0.1/# ServerActive=$ZABIP" /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# EnableRemoteCommands=0/EnableRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# LogRemoteCommands=0/LogRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/Server=127.0.0.1/Server=$ZABIP/" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/ServerActive=127.0.0.1/# ServerActive=127.0.0.1/" /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
 # sed -i "s/# SourceIP=/SourceIP=$AGIP/" /etc/zabbix/zabbix_agentd.conf
 # sed -i "s/# ListenIP=0.0.0.0/ListenIP=$AGIP/" /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i "s/# ListenPort=10050/ListenPort=$PORT/" /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i 's/# UnsafeUserParameters=0/UnsafeUserParameters=1/' /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i 's/# TLSConnect=unencrypted/TLSConnect=psk/' /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i 's/# TLSAccept=unencrypted/TLSAccept=psk/' /etc/zabbix/zabbix_agentd.conf
-  sudo sed -i "s/# TLSPSKIdentity=/TLSPSKIdentity=PropertiesFile/" /etc/zabbix/zabbix_agentd.conf
-echo "TLSPSKFile=/etc/zabbix/PropertiesFile" >> /etc/zabbix/zabbix_agentd.conf
-echo "PropertiesFile" >> /etc/zabbix/zabbix_agentd.psk
+  sed -i "s/# ListenPort=10050/ListenPort=$PORT/" /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# UnsafeUserParameters=0/UnsafeUserParameters=1/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# TLSConnect=unencrypted/TLSConnect=psk/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# TLSAccept=unencrypted/TLSAccept=psk/' /etc/zabbix/zabbix_agentd.conf
+  sed -i "s/# TLSPSKIdentity=/TLSPSKIdentity=purk-zabbix-exchange-001/" /etc/zabbix/zabbix_agentd.conf
+echo "TLSPSKFile=/etc/zabbix/zabbix_agentd.psk" >> /etc/zabbix/zabbix_agentd.conf
+echo "1f87b595725ac58dd977beef14b97461a7c1045b9a1c96308899ca445566aaff" >> /etc/zabbix/zabbix_agentd.psk
 
 
 # -----------8<--------customizations and userparams------>8--------------
@@ -72,11 +71,11 @@ echo "PropertiesFile" >> /etc/zabbix/zabbix_agentd.psk
 # -----------8<------------------------------------------->8-------------
 
 # Open firewall for zabbix communication
-sudo ufw allow from '$ZABIP'/32 to any port '$PORT'
-#  ufw reload
+  ufw allow from $ZABIP/32 to any port $PORT
+  ufw reload
 
 #Restarting zabbix agent
-sudo systemctl restart zabbix-agent
+  systemctl restart zabbix-agent
 
 # Preparing details file for zabbix server
 echo $HOSTNAME >> ./hostname.txt
